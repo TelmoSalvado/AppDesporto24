f6041c60d20810125835633754f4a9269aee0785
TelmoSalvado <telmosalvado18@gmail.com>
2019-03-20 19:11:34 +0000

    Inserção dos botões na aplicação

--- app/src/main/java/pt/ipg/desporto24/MainActivity.java
+++ app/src/main/java/pt/ipg/desporto24/MainActivity.java
@@ -2,6 +2,8 @@ package pt.ipg.desporto24;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void zonaFutebol(View view){
+        Toast.makeText(this, "Zona de Futebol em desenvolvimento!! Nesta Zona irás ter acesso a todas as novidades do Futebol!!",
+                Toast.LENGTH_LONG).show();
+        
+    }
+    public void zonaFutsal(View view){
+        Toast.makeText(this, "Zona de Futsal em desenvolvimento!! Nesta Zona irás ter acesso a todas as novidades do Futsal!!",
+                Toast.LENGTH_LONG).show();
+
+    }
+    public void zonaHoquei(View view){
+        Toast.makeText(this, "Zona de Hoquei em desenvolvimento!! Nesta Zona irás ter acesso a todas as novidades do Hoquei!!",
+                Toast.LENGTH_LONG).show();
+
+    }
+    public void zonaAndebol(View view){
+        Toast.makeText(this, "Zona de Andebol em desenvolvimento!! Nesta Zona irás ter acesso a todas as novidades do Andebol!!",
+                Toast.LENGTH_LONG).show();
+
+    }
+    public void zonaTenis(View view){
+        Toast.makeText(this, "Zona de Tenis em desenvolvimento!! Nesta Zona irás ter acesso a todas as novidades do Tenis!!",
+                Toast.LENGTH_LONG).show();
+
+    }
+    public void zonaNoticias(View view){
+        Toast.makeText(this, "Zona de Noticias em desenvolvimento!! Aqui poderás aceder a todas as noticias do mundo do desporto!!",
+                Toast.LENGTH_LONG).show();
+
     }
 }
